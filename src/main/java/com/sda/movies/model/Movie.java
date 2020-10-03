b99b6b6b3a765629aea2168642e9f50d4d21c4d4
lukasz <lukasz.stojecki@o2.pl>
2020-10-03 10:26:44 +0200

    added movie model

--- src/main/java/com/sda/movies/model/Movie.java
+++ src/main/java/com/sda/movies/model/Movie.java
@@ -1,4 +1,30 @@
 package com.sda.movies.model;
 
 public class Movie {
+
+    private Integer id;
+    private String title;
+
+    private static Integer currentId = 0;
+
+    public Movie(String title) {
+        this.id = currentId++;
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
